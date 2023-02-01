ca296fd62c797cd6401ba89256b53f71afd56cf7
miguel <74852114+mickeygcodes@users.noreply.github.com>
2023-01-31 18:41:56 -0800

    Fix start script/ add build. Updated corsConfig with hosting frontend url

--- server/configs/corsConfig.ts
+++ server/configs/corsConfig.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fastpack.onrender.com",
+];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
