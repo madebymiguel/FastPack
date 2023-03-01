fd53bbf883138d0ae18beae4379cc727a646323e
miguel <74852114+mickeygcodes@users.noreply.github.com>
2023-03-01 03:34:38 -0800

    Update Inventory model

--- server/models/Inventory.ts
+++ server/models/Inventory.ts
@@ -10,17 +10,17 @@ export interface InventoryType {
   isConsumable: boolean;
   createdBy: { type: Types.ObjectId };
   // For PackList
-  isWorn: boolean | null;
-  tripQuantity: number | null;
+  isWorn: boolean;
+  tripQuantity: number;
 }
 
 const InventorySchema = new Schema<InventoryType>({
-  name: { type: String },
+  name: { type: String, required: [true, "Please Provide Item Name"] },
   description: { type: String },
-  category: { type: String },
-  weight: { type: Number },
-  weightUnit: { type: String },
-  quantity: { type: Number },
+  category: { type: String, required: [true, "Please Provide Category"] },
+  weight: { type: Number, required: [true, "Please Provide Weight"] },
+  weightUnit: { type: String, required: [true, "Please Provide Weight Unit"] },
+  quantity: { type: Number, required: [true, "Please Provide Quantity"] },
   isConsumable: { type: Boolean },
   createdBy: {
     type: Types.ObjectId,
@@ -28,8 +28,8 @@ const InventorySchema = new Schema<InventoryType>({
     required: [true, "Please Provide User"],
   },
   // For PackList
-  isWorn: { type: Boolean, default: null },
-  tripQuantity: { type: Number, default: null },
+  isWorn: { type: Boolean, default: false },
+  tripQuantity: { type: Number, default: 1 },
 });
 
 export default model<InventoryType>("Inventory", InventorySchema);
